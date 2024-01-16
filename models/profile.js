7a3e3d91b820d1915f129a8ce57cfe77127fa7fb
mizutee <chrisansenw96@gmail.com>
2024-01-16 23:22:20 +0700

    Migration has been run successfully

--- models/profile.js
+++ models/profile.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Profile.belongsTo(models.User, {references: "UserId"})
+      Profile.belongsTo(models.User, {foreignKey: "UserId"})
     }
   }
   Profile.init(
--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasOne(models.Profile, {references: "UserId"})
+      User.hasOne(models.Profile, {foreignKey: "UserId"})
     }
   }
   User.init(
