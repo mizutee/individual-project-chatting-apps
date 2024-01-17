17d3316c6909c5c84640f744bca0cec29e9ffc67
mizutee <chrisansenw96@gmail.com>
2024-01-17 11:10:16 +0700

    Created seeding js

---
+++ seeders/20240117040804-user-input.js
@@ -0,0 +1,34 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = require('../data/user.json').map(el => {
+    el.createdAt = el.updatedAt = new Date()
+    el.password = bcrypt.hashSync(el.password, bcrypt.genSaltSync(8))
+    return el;
+   });
+
+   await queryInterface.bulkInsert("Users", data, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  }
+};
